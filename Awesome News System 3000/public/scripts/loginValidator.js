e2cef72022db26931a905a8fd79eff8626230fd8
DimitarDKirov <dimkirov@hotmail.com>
2016-12-02 11:42:28 +0200

    Added validation on registration, change username pattern

--- Awesome News System 3000/public/scripts/loginValidator.js
+++ Awesome News System 3000/public/scripts/loginValidator.js
@@ -1,13 +1,15 @@
 /*globals $ toastr CryptoJS*/
 (function () {
-    const UsernameRegex = /^[a-zA-Z0-9]{5,30}$/;
+    const UsernameRegex = /^[a-zA-Z0-9]{3,30}$/;
     const PasswordRegex = /^[^<>?$]{5,30}$/;
-    const EmailRegex = /^[A-Z0-9._%+-]+@(?:[A-Z0-9-]+\.)+[A-Z]{2,}$/;
+    const EmailRegex = /^[a-z0-9._%+-]+@(?:[a-z0-9-]+\.)+[a-z]{2,}$/;
 
     $('#authenticate').submit(function (event) {
+        toastr.remove();
+
         let username = $('#username').val();
         if (!UsernameRegex.test(username)) {
-            toastr.error('Username must contain between 5 and 30 letters or digits');
+            toastr.error('Username must contain between 3 and 30 letters or digits');
             event.preventDefault();
         }
 
