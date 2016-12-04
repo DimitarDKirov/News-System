8a94b7f04dede7b988f8464e0fc209c1050e913c
mkjordanov <mkjordanov@gmail.com>
2016-12-04 15:15:21 +0200

    HTML characters are handled in search

--- Awesome News System 3000/controllers/search-article-controller.js
+++ Awesome News System 3000/controllers/search-article-controller.js
@@ -3,8 +3,14 @@
 module.exports = function(data) {
     return {
         searchArticles(req, res) {
-            if (!req.query.search === "") {
-                data.getSimpleArticleByName(req.query.search)
+            if (req.query.search.length !== 0) {
+                let searchPhrase = req.query.search
+                    .replace(/&/g, "&amp;")
+                    .replace(/</g, "&lt;")
+                    .replace(/>/g, "&gt;")
+                    .replace(/"/g, "&quot;")
+                    .replace(/'/g, "&#039;");
+                data.getSimpleArticleByName(searchPhrase)
                     .then(article => {
                         res.render("../views/articles/simple-article-list", {
                             result: article,
